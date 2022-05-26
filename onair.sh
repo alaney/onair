0a16f35d8c20d109aac352ef85eca24cfb534829
Aaron Laney <aaron.laney@adhocteam.us>
2022-05-26 14:36:07 -0500

    it's alive

--- onair.sh
+++ onair.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+signOn=0
+
 while true
 do
   parent_path=$(dirname "${BASH_SOURCE[0]}")
@@ -7,9 +9,33 @@ do
   zoomCount=$(ps aux | grep -c zoom)
 
   if [[ $isMeetOpen -eq 1 || $zoomCount -gt 3 ]]; then
-    echo 1
-  else
-    echo 0
+    if [[ $signOn -eq 0 ]]; then
+      signOn=1
+      curl --location --request POST 'http://192.168.87.21:8081/zeroconf/switch' \
+        --header 'Content-Type: application/json' \
+        --data-raw '{
+            "sequence": "",
+            "encrypt": "false",
+            "selfApikey": "",
+            "deviceid": "10013c757a",
+            "data": {
+                "switch": "on"
+            }
+        }'
+      fi
+  elif [[ $signOn -eq 1 ]]; then
+    signOn=0
+    curl --location --request POST 'http://192.168.87.21:8081/zeroconf/switch' \
+      --header 'Content-Type: application/json' \
+      --data-raw '{
+          "sequence": "",
+          "encrypt": "false",
+          "selfApikey": "",
+          "deviceid": "10013c757a",
+          "data": {
+              "switch": "off"
+          }
+      }'
   fi 
   sleep 10
 done
